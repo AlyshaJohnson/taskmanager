013c681a0b289468e7290aa0c5772d54227624da
AlyshaJohnson <alysha.johnson@live.co.uk>
2022-10-08 07:06:28 +0000

    initial commit

---
+++ taskmanager/models.py
@@ -0,0 +1,30 @@
+# database schema
+
+from taskmanager import db
+
+
+class Category(db.Model):
+    # schema for the category model
+    id = db.Column(db.Integer, primary_key=True)
+    category_name = db.Column(db.String(25), unique=True, nullable=False)
+    tasks = db.relationship("Task", backref="category", cascade="all, delete", lazy=True)  # noqa
+
+    def __repr__(self):
+        # __repr__ to represent itself as a string
+        return self.category_name
+
+
+class Task(db.Model):
+    # schema for the task model
+    id = db.Column(db.Integer, primary_key=True)
+    task_name = db.Column(db.String(50), unique=True, nullable=False)
+    task_description = db.Column(db.Text, nullable=False)
+    is_urgent = db.Column(db.Boolean, default=False, nullable=False)
+    due_date = db.Column(db.Date, nullable=False)
+    category_id = db.Column(db.Integer, db.ForeignKey("category.id", ondelete="CASCADE"), nullable=False)  # noqa
+
+    def __repr__(self):
+        # __repr__ to represent itself as a string
+        return "#{0} - Task: {1} | Ugrent: {2}".format(
+            self.id, self.task_name, self.is_urgent
+        )
