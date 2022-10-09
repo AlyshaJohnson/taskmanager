542152a9aa765acae710ed16dffec9e83a4d81f9
AlyshaJohnson <alysha.johnson@live.co.uk>
2022-10-09 15:18:12 +0000

    add add_task page and connect to database

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,5 +1,16 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // sidenav initialisation
-    var sidenav = document.querySelectorAll('.sidenav');
-    M.Sidenav.init(sidenav);
+document.addEventListener('DOMContentLoaded', function () {
+  // sidenav initialisation
+  var sidenav = document.querySelectorAll('.sidenav');
+  M.Sidenav.init(sidenav);
+
+  // date picker initialisation
+  var datepicker = document.querySelectorAll('.datepicker');
+  M.Datepicker.init(datepicker, {
+    format: "dd mmm, yyyy",
+    i18n: {done: "Select"}
   });
+
+  // drop down category select
+  var category_select = document.querySelectorAll('select');
+  M.FormSelect.init(category_select);
+});
